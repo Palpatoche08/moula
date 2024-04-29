bfa8bc2f928ed7ea29f5d14cd17db13c3ca51e20
Thomas Scheepers <121402966+Palpatoche08@users.noreply.github.com>
2024-04-29 11:20:09 -0700

    thomas account

---
+++ app/src/main/java/com/example/moulaproject/DeleteAccountActivity.java
@@ -0,0 +1,24 @@
+package com.example.moulaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.moulaproject.Database.UserDAO;
+
+public class DeleteAccountActivity extends AppCompatActivity {
+
+    UserDAO userDAO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+
+
+    }
+}
