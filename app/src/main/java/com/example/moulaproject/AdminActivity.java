0a99e736de1d6311fc1b6c28c7566e888c8e1685
NCH04 <nabilchartouni1@gmail.com>
2024-04-29 21:43:35 +0300

    thomas changement

--- app/src/main/java/com/example/moulaproject/AdminActivity.java
+++ app/src/main/java/com/example/moulaproject/AdminActivity.java
@@ -1,9 +1,13 @@
+
 package com.example.moulaproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.moulaproject.Database.UserRepo;
@@ -14,6 +18,7 @@ import java.util.List;
 public class AdminActivity extends AppCompatActivity {
 
     private UserRepo db;
+    private TextView listUserTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,26 @@ public class AdminActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin);
 
         this.db = new UserRepo(getApplication());
+        listUserTextView = findViewById(R.id.listUser);
+
         StringBuilder list = new StringBuilder();
 
         List<User> users = db.getAllLogs();
         for(User u: users){
             list.append(u.getName()).append("\n");
         }
+        listUserTextView.setText(list.toString());
 
+        Button userRemove = findViewById(R.id.user_remove);
+        userRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminActivity.this, DeleteAccountActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+
 }
